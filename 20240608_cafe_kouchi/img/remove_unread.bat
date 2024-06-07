9ecdd77e5205fca6c12e959ec44d8302acfffdb3
Daisuke Motohashi <ddssk.m@gmail.com>
2024-06-07 15:45:12 +0900

    kintone Café高知のスライドを追加

---
+++ 20240608_cafe_kouchi/img/remove_unread.bat
@@ -0,0 +1,15 @@
+@echo off
+del /Q /F "charactors_s.png"
+del /Q /F "chara_browserextension_s.png"
+del /Q /F "circle_kintonecafe.png"
+del /Q /F "coderdojo_logo_full_trans.png"
+del /Q /F "coderdojo_ninjas_s.png"
+del /Q /F "family.png"
+del /Q /F "json-downloaded.png"
+del /Q /F "kintanlab.png"
+del /Q /F "kms_table_s.png"
+del /Q /F "KMS_特別授業01_s.png"
+del /Q /F "アイコン全部DLする.png"
+del /Q /F "ダミーデータ作成.png"
+echo All specified files have been deleted.
+pause
